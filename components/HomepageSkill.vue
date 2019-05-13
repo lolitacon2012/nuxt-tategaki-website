c521171f1c7f832e9655fbe19c9d5b391736850d
Liu Dake <dake.liu@shopee.com>
2019-05-13 20:59:35 +0800

    ! make icon smaller on mobile

--- components/HomepageSkill.vue
+++ components/HomepageSkill.vue
@@ -32,40 +32,16 @@ export default {
   height: 5rem;
 }
 
-.tool-box-item > h1 {
-  font-size: 5rem;
-}
-
-.tool-box-item > h2 {
-  font-size: 1.2rem;
-  font-weight: 100;
-}
-
-.tool-box-item > h3 {
-  font-size: 0.8rem;
-  font-weight: 100;
-}
 @media (max-width: 768px) {
   .tool-box-item {
     padding: 1rem 0;
     text-align: center;
     width: 33.333333333333333333333%;
   }
-
-  .tool-box-item > h1 {
-    font-size: 4rem;
+  .tool-box-item > .tool-box-item-icon {
+    width: 3.5rem;
+    height: 3.5rem;
   }
-
-  .tool-box-item > h2 {
-    font-size: 1rem;
-    font-weight: 100;
-  }
-
-  .tool-box-item > h3 {
-    font-size: 0.8rem;
-    font-weight: 100;
-  }
-
   .hide-on-mobile {
     display: none;
   }
